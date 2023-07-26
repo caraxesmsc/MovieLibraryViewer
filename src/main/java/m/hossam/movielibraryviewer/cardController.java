d3ee02a0151ebedf283ea9542f6ff33a1a2221ad
caraxesmsc <mohamedongo21@gmail.com>
2023-07-26 03:55:31 +0300

    code for Recently added movies

--- src/main/java/m/hossam/movielibraryviewer/cardController.java
+++ src/main/java/m/hossam/movielibraryviewer/cardController.java
@@ -1,5 +1,35 @@
 package m.hossam.movielibraryviewer;
 
-public class cardController{
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import model.Movie;
 
+public class cardController{
+    @FXML
+    private HBox box;
+
+    @FXML
+    private Label movieGenre;
+
+    @FXML
+    private Label movieName;
+
+    @FXML
+    private ImageView moviePoster;
+
+    @FXML
+    private Label movieRating;
+    private String[] colors = {"6A1B9A","8E24AA","8E24AA"};
+    public void setData(Movie movie){
+        Image image = new Image(getClass().getResourceAsStream(movie.getImgsrc()));
+        moviePoster.setImage(image);
+        movieName.setText(movie.getName());
+        movieGenre.setText(movie.getGenre());
+        movieRating.setText(movie.getRating());
+        box.setStyle("-fx-background-color: "+ Color.web(colors[(int)Math.random()*colors.length]));
+    }
 }
--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -1,4 +1,60 @@
 package model;
 
+import java.util.Date;
+
 public class Movie {
+    private String name;
+    private String genre;
+    private String rating;
+    private String imgsrc;
+    private Date dateAdded;
+    private Boolean watched;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getImgsrc() {
+        return imgsrc;
+    }
+
+    public void setImgsrc(String imgsrc) {
+        this.imgsrc = imgsrc;
+    }
+
+    public Boolean getWatched() {
+        return watched;
+    }
+
+    public void setWatched(Boolean watched) {
+        this.watched = watched;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
 }
