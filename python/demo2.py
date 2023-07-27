abf4e8cfcb9d4257ef7007c85d683f11420db88d
caraxesmsc <mohamedongo21@gmail.com>
2023-07-28 02:55:30 +0300

    another try

--- python/demo2.py
+++ python/demo2.py
@@ -1,5 +1,5 @@
 import subprocess
-
+metadata=None
 def get_video_metadata(file_path):
     try:
         cmd = [r'C:\Users\mdzhs\OneDrive\Documents\Programming Projects\MovieLibraryViewer\MovieLibraryViewer\python\exiftool.exe', file_path]
@@ -21,11 +21,23 @@ desired_fields = ['File Name', 'File Modification Date/Time', 'Genre', 'Rating',
 # Get the video file name without the extension
 video_name = video_file.split("/")[-1].split(".")[0]
 
+# Assuming metadata, desired_fields, and video_name are correctly defined
+
+# Split metadata into an array of lines
+metadata_lines = metadata.splitlines()
+
+# Create an array to store the desired metadata lines
+desired_metadata = []
+
+# Loop through each line of metadata
+for line in metadata_lines:
+    for field in desired_fields:
+        if line.startswith(field + ':'):
+            metadata_value = line.split(":")[1].strip()  # Extract metadata value after the ':'
+            desired_metadata.append(metadata_value)
+
 # Create the text file and write the desired metadata
 file_path = f"{video_name}.txt"
 with open(file_path, 'w') as file:
-    for line in metadata.splitlines():
-        for field in desired_fields:
-            if line.startswith(field + ':'):
-                metadata_value = line.split(":")[1].strip()  # Extract metadata value after the ':'
-                file.write(metadata_value + '\n')
+    for line in desired_metadata:
+        file.write(line + '\n')
