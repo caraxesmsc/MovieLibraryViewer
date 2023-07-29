66fd0038086ec4a5edd91cc7d60ee3bc4907c65e
caraxesmsc <mohamedongo21@gmail.com>
2023-07-29 19:53:38 +0300

    Still not working

--- python/demo6.py
+++ python/demo6.py
@@ -12,38 +12,35 @@ def get_video_metadata(file_path):
         return None
 
 def extract_metadata_for_directory(video_directory):
-    # List all files in the directory
+    #*** List all files in the directory
     files = os.listdir(video_directory)
 
-    # Filter out video files (you can customize this based on your video file extensions)
+    #** Filter out video files (you can customize this based on your video file extensions)
     video_files = [file for file in files if file.endswith('.mp4')]
 
-    # Assuming desired_fields are correctly defined here
+    #*** Assuming desired_fields are correctly defined here
     desired_fields = ['File Name                       ', 'File Creation Date/Time         ', 'Genre                           ', 'Comment                         ', 'Category                        ', 'Artist                          ']
 
     for video_file in video_files:
         video_name = os.path.splitext(video_file)[0]
-
-        # Proceed to create the .txt file
-        print(f"Creating {video_name}.txt")
         video_file_path = os.path.join(video_directory, video_file)
-        metadata = get_video_metadata(video_file_path)
-        if metadata:
-            metadata_lines = metadata.splitlines()
-            desired_metadata_values = []
+        print(f"Creating {video_name}.txt")
+        with open(video_file_path + '.txt', 'w') as file:
+            metadata = get_video_metadata(video_file_path)
+            if metadata:
+                metadata_lines = metadata.splitlines()
+                desired_metadata_values = []
 
-            for line in metadata_lines:
-                for field in desired_fields:
-                    if line.startswith(field + ':'):
-                        metadata_value = line.split(":")[1].strip()
-                        desired_metadata_values.append(metadata_value)
+                for line in metadata_lines:
+                    for field in desired_fields:
+                        if line.startswith(field + ':'):
+                            metadata_value = line.split(":")[1].strip()
+                            desired_metadata_values.append(metadata_value)
 
-            file_path = os.path.join(video_directory, f"{video_name}.txt")
-            with open(file_path, 'w') as file:
                 for value in desired_metadata_values:
                     file.write(value + '\n')
 
-# Example usage for a directory
+#*** Example usage for a directory
 video_directory = r'C:\\My_Data\\Movies'
-# The output_folder argument is no longer needed
+#*** The output_folder argument is no longer needed
 extract_metadata_for_directory(video_directory)
